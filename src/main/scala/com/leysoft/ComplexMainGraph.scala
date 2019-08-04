8f5ed8da0e71c2237e509ef60a5b3ff30f8de4bd
Will Leyton <wleylop22@gmail.com>
2019-08-03 20:28:09 -0500

    Add scala logging

---
+++ src/main/scala/com/leysoft/ComplexMainGraph.scala
@@ -0,0 +1,42 @@
+package com.leysoft
+
+import akka.actor.ActorSystem
+import akka.stream.{ActorMaterializer, ClosedShape, UniformFanInShape}
+import akka.stream.scaladsl.{GraphDSL, RunnableGraph, Sink, Source, ZipWith}
+import Math.max
+
+import com.typesafe.scalalogging.Logger
+import org.slf4j.LoggerFactory
+
+object ComplexMainGraph extends App {
+  implicit val system = ActorSystem("complex-graph-system")
+  implicit val materializer = ActorMaterializer()
+  val logger = Logger(LoggerFactory.getLogger("ComplexMainGraph"))
+
+  val s1 = Source.single(1)
+  val s2 = Source.single(3)
+  val s3 = Source.single(2)
+  val subscriber = Sink.foreach[Int] { i => logger.info(s"$i") }
+
+  val partialGraph = GraphDSL.create() { implicit build =>
+    import GraphDSL.Implicits._
+    // compare z1.in0 and z1.in1
+    val z1 = build.add(ZipWith[Int, Int, Int] { max })
+    // compare z2.in0(z1.out) and z2.in1
+    val z2 = build.add(ZipWith[Int, Int, Int] { max })
+    z1.out ~> z2.in0
+    UniformFanInShape(z2.out, z1.in0, z1.in1, z2.in1)
+  }
+
+  val graph = RunnableGraph.fromGraph(GraphDSL.create() { implicit build =>
+    import GraphDSL.Implicits._
+    // use partialGraph
+    val partial = build.add(partialGraph)
+    s1 ~> partial.in(0)
+    s2 ~> partial.in(1)
+    s3 ~> partial.in(2)
+    partial.out ~> subscriber
+    ClosedShape
+  })
+  graph.run()
+}
--- src/main/scala/com/leysoft/MainGraph.scala
+++ src/main/scala/com/leysoft/MainGraph.scala
@@ -2,8 +2,8 @@ package com.leysoft
 
 import akka.{Done, NotUsed}
 import akka.actor.ActorSystem
-import akka.stream.{ActorMaterializer, ClosedShape}
-import akka.stream.scaladsl.{Broadcast, Flow, GraphDSL, Merge, RunnableGraph, Sink, Source, Zip}
+import akka.stream.{ActorMaterializer, ClosedShape, FlowShape, SinkShape, SourceShape}
+import akka.stream.scaladsl.{Broadcast, Concat, Flow, GraphDSL, RunnableGraph, Sink, Source, Zip}
 import com.typesafe.scalalogging.Logger
 import org.slf4j.LoggerFactory
 
@@ -13,6 +13,7 @@ object MainGraph extends App {
   implicit val system = ActorSystem("graph-system")
   implicit val materializer = ActorMaterializer()(system)
   val logger = Logger(LoggerFactory.getLogger("MainGraph"))
+
   /*
   val input = Source(1 to 10)
   val incrementer = Flow[Int].map { i => i + 1 }
@@ -37,20 +38,11 @@ object MainGraph extends App {
   output.async
   graph.run()*/
 
-  val g = graphWithBroadcastAndZip(Source.single(2), Flow[Int].map { i =>
-    logger.info(s"${Thread.currentThread().getName}")
-    i > 0
-  }, Flow[Int].map {i =>
-    logger.info(s"${Thread.currentThread().getName}");
-    i % 2 == 0
-  }, Flow[(Boolean, Boolean)].map { t => t._1 && t._2 }, Sink.foreach(i => logger.info(s"$i")))
-  g.run()
-
   def graphWithBroadcastAndZip(in: Source[Int, NotUsed], f1: Flow[Int, Boolean, NotUsed],
                                f2: Flow[Int, Boolean, NotUsed], f3: Flow[(Boolean, Boolean), Boolean, NotUsed],
                                out: Sink[Boolean, Future[Done]]): RunnableGraph[NotUsed] =
     RunnableGraph.fromGraph(GraphDSL
-      .create() { implicit builder: GraphDSL.Builder[NotUsed] =>
+      .create() { implicit builder =>
         import GraphDSL.Implicits._
         val broadcast = builder.add(Broadcast[Int](2))
         val zip = builder.add(Zip[Boolean, Boolean])
@@ -60,4 +52,47 @@ object MainGraph extends App {
         zip.out ~>  f3.async ~> out
         ClosedShape
       })
+  val g = graphWithBroadcastAndZip(Source.single(2), Flow[Int].map { i =>
+    logger.info(s"${Thread.currentThread().getName}")
+    i > 0
+  }, Flow[Int].map {i =>
+    logger.info(s"${Thread.currentThread().getName}");
+    i % 2 == 0
+  }, Flow[(Boolean, Boolean)].map { t => t._1 && t._2 }, Sink.foreach(i => logger.info(s"$i")))
+  //g.run()
+
+  def sourceGraph[A](s1: Source[A, _], s2: Source[A, _]) : Source[A, _] =
+    Source.fromGraph(GraphDSL.create() { implicit builder =>
+      import GraphDSL.Implicits._
+      val concat = builder.add(Concat[A](2))
+      s1.async ~> concat.in(0)
+      s2.async ~> concat.in(1)
+      SourceShape(concat.out)
+  })
+  val s = sourceGraph(Source.single(1), Source.single(2))
+  //s.to(Sink.foreach { i => logger.info(s"Data: $i") }).run()
+
+  def flowGraph[A, B](f1: Flow[A, A, _], f2: Flow[A, B, NotUsed]): Flow[A, (A, B), _] =
+    Flow.fromGraph(GraphDSL.create() { implicit builder: GraphDSL.Builder[NotUsed] =>
+      import GraphDSL.Implicits._
+      val broadcast = builder.add(Broadcast[A](2))
+      val zip = builder.add(Zip[A, B])
+      broadcast.out(0) ~> f1 ~> zip.in0
+      broadcast.out(1) ~> f2 ~> zip.in1
+      FlowShape(broadcast.in, zip.out)
+    })
+  val f = flowGraph(Flow[Int].map {i => i + 1 }, Flow[Int].map {i => s"N-$i" })
+  //Source.single(2).via(f).runWith(Sink.foreach { i => logger.info(s"(${i._1}, ${i._2})") })
+
+  def sinkGraph[A](s1: Sink[A, Future[Done]], s2: Sink[A, Future[Done]]): Sink[A, NotUsed] =
+    Sink.fromGraph(GraphDSL.create() { implicit builder =>
+      import GraphDSL.Implicits._
+      val broadcast = builder.add(Broadcast[A](2))
+      broadcast ~> s1
+      broadcast ~> s2
+      SinkShape(broadcast.in)
+    })
+  val sk = sinkGraph(Sink.foreach[Int](i => logger.info(s"1: $i")),
+    Sink.foreach[Int](i => logger.info(s"2: $i")))
+  //Source.single(55).runWith(sk)
 }
